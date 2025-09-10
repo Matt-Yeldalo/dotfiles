167cf150287cfbc6f2ec4bcd5ba66e943f371878
Matt Yeldalo <matman370@hotmail.com>
2025-09-10 21:47:41 +1000

    removed bs

--- dot_config/nvim/lua/matt/plugins/quick-note.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'matt-yeldalo/quick-note',
-  -- dir = '~/projects/quick-note/',
-  config = function()
-    require('quick-note').setup({
-    })
-  end,
-}
--- dot_config/nvim/lua/matt/plugins/theme.lua
+++
@@ -1,3 +0,0 @@
-local theme = require 'matt.config.cappuccino2'
--- vim.cmd.colorscheme 'catppuccin-macchiato'
-return { theme }
