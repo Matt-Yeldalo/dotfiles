3207dfceb194831ded036e9f634e4ab783e80561
Matt Yeldalo <matt.yeldalo@outlook.com>
2025-12-10 21:49:11 +1100

    picker oil fix

--- nvim/.config/nvim/lua/matt/plugins/snacks.lua
+++ nvim/.config/nvim/lua/matt/plugins/snacks.lua
@@ -2,6 +2,24 @@ return {
   'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
+  config = function(_, opts)
+    require('snacks').setup(opts)
+
+    -- MonkeyPatch - https://github.com/folke/snacks.nvim/pull/2012
+    local M = require 'snacks.picker.core.main'
+    M.new = function(opts)
+      opts = vim.tbl_extend('force', {
+        float = false,
+
+        file = true,
+        current = false,
+      }, opts or {})
+      local self = setmetatable({}, M)
+      self.opts = opts
+      self.win = vim.api.nvim_get_current_win()
+      return self
+    end
+  end,
   ---@type snacks.Config
   opts = {
     bigfile = { enabled = true },
