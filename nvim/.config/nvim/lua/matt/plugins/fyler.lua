b0290d70add808a0b4e558922a2de13b7bcc384f
Matt Yeldalo <matt.yeldalo@outlook.com>
2026-05-02 19:17:08 +1000

    added fyler

---
+++ nvim/.config/nvim/lua/matt/plugins/fyler.lua
@@ -0,0 +1,11 @@
+return {
+  'A7Lavinraj/fyler.nvim',
+  dependencies = { 'nvim-mini/mini.icons' },
+  branch = 'stable',
+  lazy = false,
+  opts = {},
+  keys = {
+    { '-', '<Cmd>Fyler<Cr>', desc = 'Open Fyler View' },
+  },
+}
+
