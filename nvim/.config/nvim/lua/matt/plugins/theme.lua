3cfcd9c974ae4d3ec26ec56c9b80a03b40d2c0a5
Matt Yeldalo <matt.yeldalo@outlook.com>
2026-05-06 18:13:46 +1000

    update theme - trans

--- nvim/.config/nvim/lua/matt/plugins/theme.lua
+++ nvim/.config/nvim/lua/matt/plugins/theme.lua
@@ -3,11 +3,18 @@ return {
   priority = 1000,
   config = function()
     require('onedarkpro').setup {
+      options = {
+        cursorline = false, -- Use cursorline highlighting?
+        transparency = true, -- Use a transparent background?
+        terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
+        lualine_transparency = true, -- Center bar transparency?
+        highlight_inactive_windows = true, -- When the window is out of focus, change the normal background?
+      },
       styles = {
         comments = 'italic',
         keywords = 'bold,italic',
         functions = 'bold',
-      }
+      },
     }
     vim.cmd 'colorscheme vaporwave'
   end,
